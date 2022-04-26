24bbccc74fbd85c19704f1f95f9a0e3fa45e89ce
MarianaRosaluz <marianarosaluz@yahoo.com>
2022-04-25 23:14:39 -0300

    initialize repository

---
+++ api/src/main/java/br/rosaluz/banking/system/authentication/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package br.rosaluz.banking.system.authentication.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private  String name;
+    private String document;
+    private Date birthDate;
+    private String motherName;
+    private  String  login;
+    private String  password;
+
+}
